d09126eb5d981d12a923ddbab5b99c734ef9e101
lorenzoritta <marialorenzoritta1971@gmail.com>
2025-10-29 17:27:35 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="login_ok.php" method="post">
+        email: <input type="email" name="email">
+        <br>
+        senha: <input type="password" name="senha">
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
+    <div class="mb-3">
+        <div class="g-recaptcha" data-sitekey="6Ld-kvsrAAAAAPmQGHBh-Vk6FIY90pJAmQfuS76R"></div>
+    </div>
+</body>
+
+</html>
